77592dbe222c8f51f1462fffa44791cf05256af2
binertia <101157675+binertia@users.noreply.github.com>
2023-10-29 19:39:18 +0700

    first commit
    structure of fdf. left only implement logical draw in isometric

---
+++ includes/fdf.h
@@ -0,0 +1,14 @@
+#ifndef FDF_H
+# define FDF_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include "get_next_line.h"
+# include "mlx.h"
+
+char	**ft_split(char const *s, char c);
+char	*get_next_line(int fd);
+
+#endif
